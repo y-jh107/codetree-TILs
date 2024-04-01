cf51e888d7ebdcab799e01fa303606b51d8f7e0b
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-02 08:57:25 +0900

    Title: 오름 내림차순 정렬, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+
+        for(int i = 0; i < n; i ++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i ++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println();
+
+        for(int i = 0; i < n; i ++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
