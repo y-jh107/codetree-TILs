1d59cd7b2fed3e9d01724c3a097d858bb5c3c886
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-02 10:40:06 +0900

    Title: 문자열 정렬, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/문자열 정렬/string-sort.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+
+        for(int i = 0; i < arr.length; i ++) {
+            System.out.print(arr[i]);
+        }
+
+    }
+}
