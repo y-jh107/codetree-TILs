9961dca1ac689a001fc5b3aafe1d124127ec05f2
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-03 22:55:03 +0900

    Title: 요일 맞추기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240403/요일 맞추기/guess-day-of-week.java
+++ 240403/요일 맞추기/guess-day-of-week.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
-    public static String[] day = new String[] {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    //public static 
     public static int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,9 +11,20 @@ public class Main {
         int startDay = totalDays(m1, d1);
         int endDay = totalDays(m2, d2);
 
-        diff = endDay - startDay + 1;
+        diff = endDay - startDay;
+        
+        if(diff < 0) {
+            diff = Math.abs(diff);
+            int result = diff % 7;
+            String[] day = new String[] {"Mon", "Sun", "Sat", "Fri", "Thu", "Wed", "Tue"};
+            System.out.println(day[result]);
+        }
 
-        System.out.println(day[diff % 7]);
+        else {
+            int result = diff % 7;
+            String[] day = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+            System.out.println(day[result]);
+        }
     }
 
     public static int totalDays(int m, int d) {
