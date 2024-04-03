d5e7cf9fb517366c0f064a2277793bf04cbe1634
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-03 22:47:13 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240403/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String[] day = new String[] {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    public static int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt(), d1 = sc.nextInt(), m2 = sc.nextInt(), d2 = sc.nextInt(), diff = 0;
+        
+        int startDay = totalDays(m1, d1);
+        int endDay = totalDays(m2, d2);
+
+        diff = endDay - startDay + 1;
+
+        System.out.println(day[diff % 7]);
+    }
+
+    public static int totalDays(int m, int d) {
+        int passedDays = 0;
+        for(int i = 1; i < m; i ++) {
+            passedDays += days[i];
+        }
+        passedDays += d;
+
+        return passedDays;
+    }
+}
