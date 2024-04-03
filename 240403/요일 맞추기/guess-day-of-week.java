35094474b2309fd003b9bba491e3296cc8a348f7
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-03 22:56:38 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/요일 맞추기/guess-day-of-week.java
+++ 240403/요일 맞추기/guess-day-of-week.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Main {
-    //public static 
+    public static String[] day = new String[] {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
     public static int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,20 +11,12 @@ public class Main {
         int startDay = totalDays(m1, d1);
         int endDay = totalDays(m2, d2);
 
-        diff = endDay - startDay;
+        diff = endDay - startDay + 1;
         
-        if(diff < 0) {
-            diff = Math.abs(diff);
-            int result = diff % 7;
-            String[] day = new String[] {"Mon", "Sun", "Sat", "Fri", "Thu", "Wed", "Tue"};
-            System.out.println(day[result]);
-        }
+        while(diff < 0)
+            diff += 7;
 
-        else {
-            int result = diff % 7;
-            String[] day = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-            System.out.println(day[result]);
-        }
+        System.out.println(day[diff]);
     }
 
     public static int totalDays(int m, int d) {
