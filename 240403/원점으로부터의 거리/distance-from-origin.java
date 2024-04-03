d484b7de39c714cc9d68a92ab52d8df477234bc2
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-03 13:17:10 +0900

    Title: 원점으로부터의 거리, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240403/원점으로부터의 거리/distance-from-origin.java
+++ 240403/원점으로부터의 거리/distance-from-origin.java
@@ -14,6 +14,7 @@ class Point implements Comparable<Point> {
 
     @Override
     public int compareTo(Point p) {
+        if(this.dis == p.dis) return this.num - p.num;
         return this.dis - p.dis;
     }
 }
