d30e573ab41215515f742ba33905db5c9a909204
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-03 22:16:18 +0900

    Title: DateTime to DateTime, Time: 3042ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240403/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static int currentDate = 11, currentHour = 11, currentMinute = 11;
+
+    public static int countDays(int d, int h, int m) {
+        int cnt = 0;
+
+        while(true) {
+            if(currentDate == d && currentHour == h && currentMinute == m) {
+                break;
+            }
+
+            cnt ++;
+            currentMinute ++;
+
+            if(currentMinute == 60) {
+                currentHour ++;
+                currentMinute = 0;
+            }
+
+            if(currentHour == 24) {
+                currentDate ++;
+                currentHour = 0;
+            }
+        }
+
+        return cnt;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(), m = sc.nextInt(), d = sc.nextInt();
+
+        int result = countDays(h, m, d);
+        System.out.println(result);
+    }
+}
