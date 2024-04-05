8da555b905f7a103f9bb5139b3708df2584964ae
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-05 21:40:58 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240405/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+        int[] cleaned = new int[n];
+
+        for(int i = 1; i <= k; i ++) {
+            int a = sc.nextInt() - 1, b = sc.nextInt() - 1;
+            for(int j = a; j <= b; j ++) {
+                cleaned[i] += 1;
+            }
+        }
+
+        int max = 0;
+        for(int i = 0; i < n; i ++) {
+            if(cleaned[i] >= max) max = cleaned[i];
+        }
+
+        System.out.println(max);
+    }
+}
