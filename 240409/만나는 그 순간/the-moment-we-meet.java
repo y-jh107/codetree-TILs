d341bdbe10c7eb0260e6ec48f4d0fda95debc4cd
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-09 12:55:01 +0900

    Title: 만나는 그 순간, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[] arr1 = new int[2001];
+        int[] arr2 = new int[2001];
+        int current1 = 1000, current2 = 1000, stack1 = 0, stack2 = 0;
+
+        for(int i = 0; i < n; i ++) {
+            String dir = sc.next();
+            int t = sc.nextInt();
+            
+            if(dir.equals("R")) {
+                for(int j = 0; j < t; j ++){
+                    arr1[current1] = stack1;
+                    current1 += 1;
+                    stack1 += 1;
+                }
+            }
+
+            if(dir.equals("L")) {
+                for(int j = 0; j < t; j ++){
+                    arr1[current1] = stack1;
+                    current1 -= 1;
+                    stack1 += 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i ++) {
+            String dir = sc.next();
+            int t = sc.nextInt();
+            
+            if(dir.equals("R")) {
+                for(int j = 0; j < t; j ++){
+                    arr2[current2] = stack2;
+                    current2 += 1;
+                    stack2 += 1;
+                }
+            }
+
+            if(dir.equals("L")) {
+                for(int j = 0; j < t; j ++){
+                    arr2[current2] = stack2;
+                    current2 -= 1;
+                    stack2 += 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < arr1.length; i ++) {
+            if(arr1[i] != 0 && arr2[i] == arr1[i]) {
+                System.out.println(arr1[i]);
+                break;
+            }
+        }
+    }
+}
