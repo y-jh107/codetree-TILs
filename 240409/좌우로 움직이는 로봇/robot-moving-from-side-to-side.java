a2dc248003f41f5ac785720f7fc6bc653d90ff20
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-09 14:22:32 +0900

    Title: 좌우로 움직이는 로봇, Time: 974ms, Memory: 57MB, Status: Passed - CodeTree

--- 240409/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240409/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,43 +9,87 @@ public class Main {
         int n = sc.nextInt(), m = sc.nextInt();
         int aMove = 1, bMove = 1, cnt = 0;
 
-        for(int i = 0; i < n; i ++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
+        if(n > m) {
+            for(int i = 0; i < n; i ++) {
+                int t = sc.nextInt();
+                char d = sc.next().charAt(0);
 
-            while(t -- > 0) {
-                if(d == 'R') {
-                    aRobot[aMove] = aRobot[aMove - 1] + 1;
-                    aMove ++;
+                while(t -- > 0) {
+                    if(d == 'R') {
+                        aRobot[aMove] = aRobot[aMove - 1] + 1;
+                        aMove ++;
+                    }
+                    else if(d == 'L') {
+                        aRobot[aMove] = aRobot[aMove - 1] - 1;
+                        aMove ++;
+                    }
                 }
-                else if(d == 'L') {
-                    aRobot[aMove] = aRobot[aMove - 1] - 1;
-                    aMove ++;
+            }
+
+            for(int i = 0; i < m; i ++) {
+                int t = sc.nextInt();
+                char d = sc.next().charAt(0);
+
+                while(t -- > 0) {
+                    if(d == 'R') {
+                        bRobot[bMove] = bRobot[bMove - 1] + 1;
+                        bMove ++;
+                    }
+                    else if(d == 'L') {
+                        bRobot[bMove] = bRobot[bMove - 1] - 1;
+                        bMove ++;
+                    }
                 }
+            }
+
+            for(int i = bMove; i < bRobot.length; i ++) {
+                bRobot[i] = bRobot[i - 1];
             }
         }
 
-        for(int i = 0; i < m; i ++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
+        else {
+            for(int i = 0; i < n; i ++) {
+                int t = sc.nextInt();
+                char d = sc.next().charAt(0);
 
-            while(t -- > 0) {
-                if(d == 'R') {
-                    bRobot[bMove] = bRobot[bMove - 1] + 1;
-                    bMove ++;
+                while(t -- > 0) {
+                    if(d == 'R') {
+                        aRobot[aMove] = aRobot[aMove - 1] + 1;
+                        aMove ++;
+                    }
+                    else if(d == 'L') {
+                        aRobot[aMove] = aRobot[aMove - 1] - 1;
+                        aMove ++;
+                    }
                 }
-                else if(d == 'L') {
-                    bRobot[bMove] = bRobot[bMove - 1] - 1;
-                    bMove ++;
+            }
+
+            for(int i = 0; i < m; i ++) {
+                int t = sc.nextInt();
+                char d = sc.next().charAt(0);
+
+                while(t -- > 0) {
+                    if(d == 'R') {
+                        bRobot[bMove] = bRobot[bMove - 1] + 1;
+                        bMove ++;
+                    }
+                    else if(d == 'L') {
+                        bRobot[bMove] = bRobot[bMove - 1] - 1;
+                        bMove ++;
+                    }
                 }
+            }
+
+            for(int i = aMove; i < aRobot.length; i ++) {
+                    aRobot[i] = aRobot[i - 1];
             }
         }
 
-        int check = 0;
+        int check1 = 0, check2 = 0;
         for(int i = 1; i < (aMove > bMove ? aMove : bMove); i ++) {
-            if(aRobot[i] != 0 && aRobot[i] == bRobot[i] && aRobot[i] != check) {
+            //System.out.println(aRobot[i] + " " + bRobot[i]);
+            if(aRobot[i] != 0 && aRobot[i] == bRobot[i] && aRobot[i - 1] != bRobot[i - 1]) {
                 cnt ++;
-                check = aRobot[i];
             }
         }
 
