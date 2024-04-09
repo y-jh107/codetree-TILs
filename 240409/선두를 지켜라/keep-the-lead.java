8c47574ef2bcdf0db0db2e75f529c65aca2664e2
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-09 13:29:55 +0900

    Title: 선두를 지켜라, Time: 130ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240409/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int MAX_NUM = 1000;
+    public static int[] A_point = new int[MAX_NUM * 1000];
+    public static int[] B_point = new int[MAX_NUM * 1000];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        int indexA = 1, indexB = 1;
+
+        for(int i = 0; i < n; i ++) {
+            int v = sc.nextInt(), t = sc.nextInt();
+
+            while(t -- > 0) {
+                A_point[indexA ++] = v;
+            }
+        }
+
+        for(int i = 0; i < m; i ++) {
+            int v = sc.nextInt(), t = sc.nextInt();
+
+            while(t -- > 0) {
+                B_point[indexB ++] = v;
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 1; i < MAX_NUM; i ++) {
+            if(A_point[i] < B_point[i] && A_point[i - 1] > B_point[i - 1]) cnt ++;
+            if(A_point[i] > B_point[i] && A_point[i - 1] < B_point[i - 1]) cnt ++;
+        }
+
+        System.out.println(cnt);
+    }
+}
