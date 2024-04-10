390feae93ed0dcf97f0eec5c984c1a23fc6749de
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-10 21:29:21 +0900

    Title: 선두를 지켜라 3, Time: 294ms, Memory: 28MB, Status: Passed - CodeTree

--- 240410/선두를 지켜라 3/keep-the-lead-3.java
+++ 240410/선두를 지켜라 3/keep-the-lead-3.java
@@ -11,33 +11,46 @@ public class Main {
         int[] aMove = new int[1000001], bMove = new int[1000001];
         String[] winner = new String[1000001];
 
+        aMove[0] = 0;
+        bMove[0] = 0;
         for (int i = 0; i < n; i ++) {
             int v = sc.nextInt(), t = sc.nextInt();
             for (int j = 0; j < t; j ++) {
-                aMove[timePassed ++] += v;
+                aMove[timePassed] = aMove[timePassed - 1] + v;
+                //System.out.println(aMove[timePassed] + " " + timePassed);
+                timePassed ++;
            }
         }
 
         timePassed = 1;
 
-        for (int i = 1; i < m; i ++) {
+        for (int i = 0; i < m; i ++) {
             int v = sc.nextInt(), t = sc.nextInt();
             for (int j = 0; j < t; j ++) {
-                bMove[timePassed ++] += v;
+                bMove[timePassed] = bMove[timePassed - 1] + v;
+                //System.out.println(bMove[timePassed] + " " + timePassed);
+                timePassed ++;
             }
         }
 
         winner[0] = "0";
-        for (int i = 1; i < winner.length; i ++) {
+        for (int i = 1; i < timePassed; i ++) {
+            //System.out.println(aMove[i] + " " + bMove[i]);
+
             if(aMove[i] > bMove[i]) winner[i] = "a";
             else if(bMove[i] > aMove[i]) winner[i] = "b";
-            else winner[i] = winner[i - 1];
+            else if(bMove[i] == aMove[i]) winner[i] = "a,b";
+
+            //System.out.println(winner[i]);
         }
 
         int newWins = 0;
-        for (int i = 2; i < winner.length; i ++) {
-            if (winner[i] != winner[i - 1])
+        for (int i = 1; i < timePassed; i ++) {
+            if (winner[i] != winner[i - 1]) {
                 newWins ++;
+                //System.out.println(winner[i] + " " + winner[i - 1]);
+            }
+                
         }
 
         System.out.println(newWins);
