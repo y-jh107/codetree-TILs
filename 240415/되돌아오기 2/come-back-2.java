b2315de411f838a6a63e4cc06d56eed6aab78c88
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-15 12:50:33 +0900

    Title: 되돌아오기 2, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240415/되돌아오기 2/come-back-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] dx = {0, -1, 0, 1};
+    public static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sec = 0, x = 0, y = 0;
+        int dirNum = 3;
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i ++) {
+            char dir = str.charAt(i);
+
+            if(dir == 'F') {
+                x += dx[dirNum];
+                y += dy[dirNum];
+                //System.out.println(x + " " + y);
+            }
+            else if(dir == 'L') {
+                dirNum = (dirNum + 3) % 4;
+                //System.out.println(dirNum);
+            }
+            else if(dir == 'R') {
+                dirNum = (dirNum + 1) % 4;
+                //System.out.println(dirNum);
+            }
+
+            sec ++;
+            if(x == 0 && y == 0) {
+                System.out.println(sec);
+                break;
+            }
+        }
+    } 
+}
