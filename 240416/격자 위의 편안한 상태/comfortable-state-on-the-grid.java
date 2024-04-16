5c5ce18e2e0f821951855d03431980973d651b1c
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-16 13:04:16 +0900

    Title: 격자 위의 편안한 상태, Time: 286ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240416/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[][] colored = new int [101][101];
+    public static int n;
+
+    public static boolean inRange(int x, int y) {
+        return (x - 1 >= 0 && x + 1 <= n && y - 1 >= 0 && y + 1 <= n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+
+        while (m -- > 0) {
+            int r = sc.nextInt() - 1, c = sc.nextInt() - 1, colors = 0;
+            colored[r][c] = 1;
+            if(inRange(r, c)) {
+                if(colored[r - 1][c] == 1) colors ++;
+                if(colored[r + 1][c] == 1) colors ++;
+                if(colored[r][c - 1] == 1) colors ++;
+                if(colored[r][c + 1] == 1) colors ++;
+            }
+
+            if(colors >= 3) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+}
