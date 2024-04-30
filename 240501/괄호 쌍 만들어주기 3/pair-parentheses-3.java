70f8fa9d07e121f9db44dcf02f72dd36e653f55c
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-01 08:46:33 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240501/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int cnt = 0;
+
+        for(int i = 0; i < str.length(); i ++) {
+            char start = str.charAt(i);
+            for(int j = i + 1; j < str.length(); j ++) {
+                if(start == '(') {
+                    if(str.charAt(j) == ')') cnt ++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
