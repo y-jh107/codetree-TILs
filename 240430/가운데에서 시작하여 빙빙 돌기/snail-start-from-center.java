293580570d6943f7c3e7add1d56395345e2ad298
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-30 11:22:06 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 246ms, Memory: 12MB, Status: Passed - CodeTree

--- 240430/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
+++ 240430/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static int[] dx = {0, -1, 0, 1};
     public static int[] dy = {1, 0, -1, 0};
-    public static int n, dir = 0;
+    public static int n, dir = 2;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,19 +11,20 @@ public class Main {
         int[][] arr = new int[n][n];
         boolean[][] visited = new boolean[n][n];
 
-        int i = n / 2, j = n / 2, cnt = 1;
+        int i = n - 1, j = n - 1, cnt = n * n;
 
         while(true) {
             if(!visited[i][j]) {
                 visited[i][j] = true;
-                arr[i][j] = cnt ++;
+                arr[i][j] = cnt --;
             }
 
-            if(cnt > n * n) break;
+            if(cnt == 0) break;
 
             int nx = i + dx[dir];
             int ny = j + dy[dir];
 
+            //System.out.println(nx + " " + ny);
             /*if(i == n - 1 && j < n && dir == 0) dir = 1;
             else if(i == 0 && j < n && dir == 1) dir = 2;
             else if(i < n && j == 0 && dir == 2) dir = 3;*/
@@ -32,6 +33,7 @@ public class Main {
                 dir = (dir + 1) % 4;
             }
             else {
+                //System.out.println(nx + " " + ny);
                 i = nx;
                 j = ny;
             }
