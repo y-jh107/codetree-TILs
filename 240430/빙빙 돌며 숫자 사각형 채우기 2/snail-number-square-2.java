fde0c048ff68408fa16fff57d0b878af23563a71
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-04-30 09:56:16 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240430/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+    public static int n, m, dir = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int cnt = 1, i = 0, j = 0;
+        int[][] arr = new int[n][m];
+        arr[i][j] = cnt ++;
+
+        while(true) {
+            
+            
+            //System.out.println(i + " " + j);
+            if(i == n - 1 && j == 0 && dir == 0) dir = 1;
+            else if(i == n - 1 && j == m - 1 && dir == 1) dir = 2;
+            else if(i == 0 && j == m - 1 && dir == 2) dir = 3;
+
+            
+
+            if(arr[i + dx[dir]][j + dy[dir]] != 0) {
+                dir = (dir + 1) % 4;
+                //System.out.println(dir + " " + i + " " + j);
+                continue;
+            }
+            
+            i += dx[dir];
+            j += dy[dir];
+
+            arr[i][j] = cnt ++;
+            //System.out.println(i + " " + j + " " + dir);
+
+            if(cnt > n * m) break;
+        }
+
+        for(i = 0; i < n; i ++) {
+            for(j = 0; j < m; j ++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
