9248614d5ef0632902e063720289a8e2abd8cd65
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-01 12:35:49 +0900

    Title: 체크판위에서 2, Time: 3026ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240501/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static int r, c;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextInt();
+        c = sc.nextInt();
+        String[][] arr = new String[r][c];
+        int cnt = 0, x = 0, y = 0;
+
+        for(int i = 0; i < r; i ++) {
+            for(int j = 0; j < c; j ++) {
+                arr[i][j] = sc.next();
+            }
+        }
+
+        while(true) {
+            String current = arr[x][y];
+
+            for(int i = x + 1; i < r; i ++) {
+                for(int j = y + 1; j < c; j ++) {
+                    if(!(arr[i][j].equals(current))) {
+                        x = i;
+                        y = j;
+                        cnt ++;
+                        break;
+                    }
+                }
+            }
+
+            if(x == r - 1 && y == c - 1) break;
+        }
+
+        System.out.println(cnt);
+    }
+}
