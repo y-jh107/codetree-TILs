58bea6ff2ea7ea92d50cd629817c89005031199c
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-04 00:13:13 +0900

    Title: 오목, Time: 166ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240504/오목/O-mok.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class Main {
+    public static int[][] omok = new int[19][19];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int winner = 0, winX = 0, winY = 0;
+        boolean checkWin = false;
+
+        for(int i = 0; i < 19; i ++) {
+            for(int j = 0; j < 19; j ++) {
+                omok[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 19; i ++) {
+            for(int j = 0; j < 19; j ++) {
+                if(omok[i][j] != 0) {
+                    for(int k = i + 1; k < i + 5; k ++) {
+                        if(omok[k][j] != omok[i][j]) {
+                            checkWin = false;
+                            break;
+                        }
+                        checkWin = true;
+                    }
+
+                    if(checkWin) {
+                        winner = omok[i][j];
+                        winX = i + 3;
+                        winY = j + 1;
+                        break;
+                    }
+
+                    for(int k = j + 1; k < j + 5; k ++) {
+                        if(omok[i][k] != omok[i][j]) {
+                            checkWin = false;
+                            break;
+                        }
+                        checkWin = true;
+                    }
+                    if(checkWin) {
+                        winner = omok[i][j];
+                        winX = i + 1;
+                        winY = j + 3;
+                        break;
+                    }
+
+                    int add = 1;
+                    while(add < 5) {
+                        if(omok[i + add][j + add] != omok[i][j]) {
+                            checkWin = false;
+                            break;
+                        }
+                        add ++;
+                        checkWin = true;
+                    }
+
+                    if(checkWin) {
+                        winner = omok[i][j];
+                        winX = i + 3;
+                        winY = j + 2;
+                        break;
+                    }
+
+                    add = 1;
+                    while(add < 5) {
+                        if(omok[i + add][j - add] != omok[i][j]) {
+                            checkWin = false;
+                            break;
+                        }
+                        if(i - add < 0 && j - add < 0) {
+                            checkWin = false;
+                            break;
+                        }
+                        add ++;
+                        checkWin = true;
+                    }
+
+                    if(checkWin) {
+                        winner = omok[i][j];
+                        winX = i + 3;
+                        winY = j - 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(winner);
+        System.out.println(winX + " " + winY);
+    }
+}
