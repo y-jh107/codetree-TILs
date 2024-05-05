941b44564fed9507cdbc1b14ec9e2af218ab1b1e
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-05 11:55:42 +0900

    Title: 구간 중 최대 합, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240505/구간 중 최대 합/max-sum-of-subarray.java
+++ 240505/구간 중 최대 합/max-sum-of-subarray.java
@@ -11,7 +11,7 @@ public class Main {
         }
 
         int ans = 0;
-        for(int i = 0; i < n - k; i ++) {
+        for(int i = 0; i <= n - k; i ++) {
             int sum = 0;
             for(int j = i; j < i + k; j ++) {
                 sum += arr[j];
