0e7019576fcfe5a28543b94cf5f67255578dc888
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-07 15:22:17 +0900

    Title: 밭의 높이를 고르게하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240507/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
+++ 240507/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
     public static int[] height = new int[101];
     public static int[] setting = new int[101];
+
     public static int n, t, h;
 
     public static int set(int x) {
@@ -15,31 +16,26 @@ public class Main {
         t = sc.nextInt();
         h = sc.nextInt();
         int minValue = Integer.MAX_VALUE;
-
-        //System.out.println(h + " " + t);
-
+        
         for(int i = 0; i < n; i ++) {
             height[i] = sc.nextInt();
         }
 
         for(int i = 0; i < n; i ++) {
             setting[i] = set(height[i]);
-            //System.out.print(setting[i] + " ");
+            System.out.print(setting[i] + " " );
         }
 
         for(int i = 0; i < n; i ++) {
             int cnt = 0;
 
             for(int j = i; j < i + t; j ++) {
-                //System.out.print(setting[j] + " cnt: ");
                 cnt += setting[j];
-                //System.out.print(cnt + " ");
             }
-            //System.out.println();
 
             minValue = Math.min(minValue, cnt);
         }
 
-        System.out.println(minValue);
+        //System.out.println(minValue);
     }
 }
