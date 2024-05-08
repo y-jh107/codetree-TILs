422daceeb92e6feb91bf7ceb1eda2f878efe6541
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-08 21:03:42 +0900

    Title: 개발자의 능력 3, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240508/개발자의 능력 3/ability-of-developer-3.java
+++ 240508/개발자의 능력 3/ability-of-developer-3.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int minDiff = 100;
+        int minDiff = 6000000;
 
         for(int i = 0; i < 6; i ++) {
             developers[i] = sc.nextInt();
