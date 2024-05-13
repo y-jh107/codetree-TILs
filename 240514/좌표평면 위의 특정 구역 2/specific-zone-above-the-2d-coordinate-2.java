8bbd87c6a8ef1caceddd7937c70e003a0900ed46
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-14 08:44:25 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240514/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static final int MAX_INT = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int ans = MAX_INT;
+        int[][] point = new int[N][2];
+
+        for(int i = 0; i < N; i ++) {
+            for(int j = 0; j < 2; j ++) {
+                point[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < N; i ++) {
+            int[] square = new int[160001];
+
+            for(int j = 0; j < N; j ++) {
+                if(i == j) continue;
+
+                int minX = MAX_INT, minY = MAX_INT,  maxX = 1, maxY = 1;
+                for(int k = 0; k < N; k ++) {
+                    if(k == i) continue;
+
+                    minX = Math.min(minX, point[k][0]);
+                    minY = Math.min(minY, point[k][1]);
+                    maxX = Math.max(maxX, point[k][0]);
+                    maxY = Math.max(maxY, point[k][1]);
+                }
+
+                int length = Math.abs(maxX - minX), width = Math.abs(maxY - minY);
+
+                int s = length * width;
+
+                square[s] ++;
+            }
+
+            for(int j = 0; j < 160001; j ++) {
+                if(square[j] != 0) {
+                    ans = Math.min(ans, j);
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
