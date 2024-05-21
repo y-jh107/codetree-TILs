2752a095bdf2b3d9b6e2d849d895f90eb20d6974
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-21 10:30:06 +0900

    Title: 스승의 은혜 3, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240521/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240521/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -26,8 +26,9 @@ public class Main {
 
                 if(total > budget) {
                     ans --;
-                    result = Math.max(result, ans);
                 }
+
+                result = Math.max(result, ans);
             }
         }
 
