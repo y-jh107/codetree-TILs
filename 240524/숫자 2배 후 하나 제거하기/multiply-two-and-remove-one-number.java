8f1c9e0be43d50d7799528d90f70604c7372e8f7
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-24 21:29:30 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 240524/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
+++ 240524/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static int MAX_N = 100;
+    public static int MAX_N = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -28,7 +28,7 @@ public class Main {
 
                 int sumDiff = 0;
 
-                for(int k = 0; k < n - 2; k ++) {
+                for(int k = 0; k < idx - 1; k ++) {
                     sumDiff += Math.abs(remainingArr[k + 1] - remainingArr[k]);
                 }
 
