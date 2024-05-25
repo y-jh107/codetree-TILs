510ed6612dbb45881c472b573c25b44f5012656b
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-25 22:52:15 +0900

    Title: 3개의 선 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240525/3개의 선 2/three-lines-2.java
+++ 240525/3개의 선 2/three-lines-2.java
@@ -1,73 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int MAX_N = 20;  // 상한을 충분히 크게 설정
+    public static int MAX_P = 20;
     public static int n;
-    public static int[] x = new int[MAX_N];
-    public static int[] y = new int[MAX_N];
+    public static int[] x = new int[MAX_P];
+    public static int[] y = new int[MAX_P];
 
-    public static int checkX(int currX) {
-        int cnt = 0;
+    public static boolean canCoverAllPoints(int[] xLines, int[] yLines) {
+        int coveredPoints = 0;
         for (int i = 0; i < n; i++) {
-            if (x[i] == currX) cnt++;
+            boolean covered = false;
+            for (int j = 0; j < 3; j++) {
+                if (x[i] == xLines[j] || y[i] == yLines[j]) {
+                    covered = true;
+                    break;
+                }
+            }
+            if (covered) {
+                coveredPoints++;
+            }
         }
-        return cnt;
-    }
-
-    public static int checkY(int currY) {
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (y[i] == currY) cnt++;
-        }
-        return cnt;
+        return coveredPoints == n;
     }
 
     public static boolean checkThreeLines() {
-        // x와 y 좌표에 대해서 모든 조합을 확인
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    int line1 = x[i];
-                    int line2 = x[j];
-                    int line3 = x[k];
-
-                    int coveredPoints = 0;
-                    for (int p = 0; p < n; p++) {
-                        if (x[p] == line1 || x[p] == line2 || x[p] == line3) {
-                            coveredPoints++;
-                        }
-                    }
-
-                    if (coveredPoints == n) {
+                    int[] xLines = {x[i], x[j], x[k]};
+                    int[] yLines = {y[i], y[j], y[k]};
+                    if (canCoverAllPoints(xLines, yLines)) {
                         return true;
                     }
-
-                    line1 = y[i];
-                    line2 = y[j];
-                    line3 = y[k];
-
-                    coveredPoints = 0;
-                    for (int p = 0; p < n; p++) {
-                        if (y[p] == line1 || y[p] == line2 || y[p] == line3) {
-                            coveredPoints++;
-                        }
-                    }
-
-                    if (coveredPoints == n) {
+                    if (canCoverAllPoints(yLines, xLines)) {
                         return true;
                     }
                 }
             }
         }
-
         return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
