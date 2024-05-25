fd5feab01e056705a1403c920841cc321ca1b226
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-25 22:51:33 +0900

    Title: 3개의 선 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240525/3개의 선 2/three-lines-2.java
+++ 240525/3개의 선 2/three-lines-2.java
@@ -1,26 +1,66 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static int MAX_N = 10;
-    public static int MAX_P = 20;
+    public static int MAX_N = 20;  // 상한을 충분히 크게 설정
     public static int n;
-    public static int[] x = new int[MAX_P];
-    public static int[] y = new int[MAX_P];
+    public static int[] x = new int[MAX_N];
+    public static int[] y = new int[MAX_N];
 
     public static int checkX(int currX) {
         int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            if(x[i] == currX) cnt++;
+        for (int i = 0; i < n; i++) {
+            if (x[i] == currX) cnt++;
         }
         return cnt;
     }
 
     public static int checkY(int currY) {
         int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            if(y[i] == currY) cnt++;
+        for (int i = 0; i < n; i++) {
+            if (y[i] == currY) cnt++;
         }
         return cnt;
+    }
+
+    public static boolean checkThreeLines() {
+        // x와 y 좌표에 대해서 모든 조합을 확인
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int line1 = x[i];
+                    int line2 = x[j];
+                    int line3 = x[k];
+
+                    int coveredPoints = 0;
+                    for (int p = 0; p < n; p++) {
+                        if (x[p] == line1 || x[p] == line2 || x[p] == line3) {
+                            coveredPoints++;
+                        }
+                    }
+
+                    if (coveredPoints == n) {
+                        return true;
+                    }
+
+                    line1 = y[i];
+                    line2 = y[j];
+                    line3 = y[k];
+
+                    coveredPoints = 0;
+                    for (int p = 0; p < n; p++) {
+                        if (y[p] == line1 || y[p] == line2 || y[p] == line3) {
+                            coveredPoints++;
+                        }
+                    }
+
+                    if (coveredPoints == n) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
@@ -28,71 +68,15 @@ public class Main {
 
         n = sc.nextInt();
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
         }
 
-        boolean isTrue = false;
-
-        for(int i = 0; i <= MAX_N; i++) {
-            for(int j = 0; j <= MAX_N; j++) {
-                for(int k = 0; k <= MAX_N; k++) {
-                    for(int l = 0; l < 8; l++) {
-                        int result = 0;
-                        if(l == 0) {
-                            result += checkX(i);
-                            result += checkX(j);
-                            result += checkX(k);
-                        }
-                        else if(l == 1) {
-                            result += checkX(i);
-                            result += checkX(j);
-                            result += checkY(k);
-                        }
-                        else if(l == 2) {
-                            result += checkX(i);
-                            result += checkY(j);
-                            result += checkX(k);
-                        }
-                        else if(l == 3) {
-                            result += checkX(i);
-                            result += checkY(j);
-                            result += checkY(k);
-                        }
-                        else if(l == 4) {
-                            result += checkY(i);
-                            result += checkX(j);
-                            result += checkX(k);
-                        }
-                        else if(l == 5) {
-                            result += checkY(i);
-                            result += checkX(j);
-                            result += checkY(k);
-                        }
-                        else if(l == 6) {
-                            result += checkY(i);
-                            result += checkY(j);
-                            result += checkX(k);
-                        }
-                        else if(l == 7) {
-                            result += checkY(i);
-                            result += checkY(j);
-                            result += checkY(k);
-                        }
-                        if(result == n) {
-                            isTrue = true;
-                            break;
-                        }
-                    }
-                    if(isTrue) break;
-                }
-                if(isTrue) break;
-            }
-            if(isTrue) break;
+        if (checkThreeLines()) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
         }
-
-        if(isTrue) System.out.println(1);
-        else System.out.println(0);
     }
 }
