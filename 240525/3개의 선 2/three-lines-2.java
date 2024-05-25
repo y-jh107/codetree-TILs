17deec7f89d01e2fd63f625c264d7e1c81caf9e3
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-25 22:48:13 +0900

    Title: 3개의 선 2, Time: 161ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240525/3개의 선 2/three-lines-2.java
+++ 240525/3개의 선 2/three-lines-2.java
@@ -2,16 +2,39 @@ import java.util.*;
 
 public class Main {
     public static int MAX_N = 10;
+    public static int MAX_P = 20;
+    public static int n;
+    public static int[] x = new int[MAX_P];
+    public static int[] y = new int[MAX_P];
+
+    public static int checkX(int currX) {
+        int cnt = 0;
+
+        for(int i = 0; i < MAX_N; i ++) {
+            if(x[i] == currX) cnt++;
+        }
+
+        return cnt;
+    }
+
+    public static int checkY(int currY) {
+        int cnt = 0;
+
+        for(int i = 0; i < MAX_N; i ++) {
+            if(y[i] == currY) cnt++;
+        }
+
+        return cnt;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[][] points = new int[n][2];
+        n = sc.nextInt();
 
         for(int i = 0; i < n; i ++) {
-            points[i][0] = sc.nextInt();
-            points[i][1] = sc.nextInt();
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
         }
 
         boolean isTrue = false;
@@ -19,18 +42,62 @@ public class Main {
         for(int i = 0; i <= MAX_N; i ++) {
             for(int j = 0; j <= MAX_N; j ++) {
                 for(int k = 0; k <= MAX_N; k ++) {
-                    boolean canPass = true;
+                    int result = 0;
 
-                    for(int l = 0; l < n; l ++) {
-                        if(points[l][0] == i || points[l][0] == j || points[l][0] == k)
-                            canPass = true;
-                        else if(points[l][1] == i || points[l][1] == j || points[l][1] == k)
-                            canPass = true;
-                        else
-                            canPass = false;
+                    for(int l = 0; l < 8; l ++) {
+                        if(l == 0) {
+                            result += checkX(i);
+                            result += checkX(j);
+                            result += checkX(k);
+                        }
+
+                        else if(l == 1) {
+                            result += checkX(i);
+                            result += checkX(j);
+                            result += checkY(k);
+                        }
+
+                        else if(l == 2) {
+                            result += checkX(i);
+                            result += checkY(j);
+                            result += checkX(k);
+                        }
+
+                        else if(l == 3) {
+                            result += checkX(i);
+                            result += checkY(j);
+                            result += checkY(k);
+                        }
+
+                        else if(l == 4) {
+                            result += checkY(i);
+                            result += checkX(j);
+                            result += checkX(k);
+                        }
+
+                        else if(l == 5) {
+                            result += checkY(i);
+                            result += checkX(j);
+                            result += checkY(k);
+                        }
+
+                        else if(l == 6) {
+                            result += checkY(i);
+                            result += checkY(j);
+                            result += checkX(k);
+                        }
+
+                        else if(l == 7) {
+                            result += checkY(i);
+                            result += checkY(j);
+                            result += checkY(k);
+                        }
+
+                        if(result == n) {
+                            isTrue = true;
+                            break;
+                        }
                     }
-
-                    if(canPass) isTrue = true;
                 }
             }
         }
