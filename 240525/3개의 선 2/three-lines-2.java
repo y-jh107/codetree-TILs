b7dd5f4667d4cb017d25987c5b4e38993cfb22fb
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-25 22:50:30 +0900

    Title: 3개의 선 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240525/3개의 선 2/three-lines-2.java
+++ 240525/3개의 선 2/three-lines-2.java
@@ -9,21 +9,17 @@ public class Main {
 
     public static int checkX(int currX) {
         int cnt = 0;
-
-        for(int i = 0; i < MAX_N; i ++) {
+        for(int i = 0; i < n; i++) {
             if(x[i] == currX) cnt++;
         }
-
         return cnt;
     }
 
     public static int checkY(int currY) {
         int cnt = 0;
-
-        for(int i = 0; i < MAX_N; i ++) {
+        for(int i = 0; i < n; i++) {
             if(y[i] == currY) cnt++;
         }
-
         return cnt;
     }
 
@@ -32,74 +28,68 @@ public class Main {
 
         n = sc.nextInt();
 
-        for(int i = 0; i < n; i ++) {
+        for(int i = 0; i < n; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
         }
 
         boolean isTrue = false;
 
-        for(int i = 0; i <= MAX_N; i ++) {
-            for(int j = 0; j <= MAX_N; j ++) {
-                for(int k = 0; k <= MAX_N; k ++) {
-                    int result = 0;
-
-                    for(int l = 0; l < 8; l ++) {
+        for(int i = 0; i <= MAX_N; i++) {
+            for(int j = 0; j <= MAX_N; j++) {
+                for(int k = 0; k <= MAX_N; k++) {
+                    for(int l = 0; l < 8; l++) {
+                        int result = 0;
                         if(l == 0) {
                             result += checkX(i);
                             result += checkX(j);
                             result += checkX(k);
                         }
-
                         else if(l == 1) {
                             result += checkX(i);
                             result += checkX(j);
                             result += checkY(k);
                         }
-
                         else if(l == 2) {
                             result += checkX(i);
                             result += checkY(j);
                             result += checkX(k);
                         }
-
                         else if(l == 3) {
                             result += checkX(i);
                             result += checkY(j);
                             result += checkY(k);
                         }
-
                         else if(l == 4) {
                             result += checkY(i);
                             result += checkX(j);
                             result += checkX(k);
                         }
-
                         else if(l == 5) {
                             result += checkY(i);
                             result += checkX(j);
                             result += checkY(k);
                         }
-
                         else if(l == 6) {
                             result += checkY(i);
                             result += checkY(j);
                             result += checkX(k);
                         }
-
                         else if(l == 7) {
                             result += checkY(i);
                             result += checkY(j);
                             result += checkY(k);
                         }
-
                         if(result == n) {
                             isTrue = true;
                             break;
                         }
                     }
+                    if(isTrue) break;
                 }
+                if(isTrue) break;
             }
+            if(isTrue) break;
         }
 
         if(isTrue) System.out.println(1);
