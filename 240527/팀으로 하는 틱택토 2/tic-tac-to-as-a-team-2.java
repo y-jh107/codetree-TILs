2924fb08be6fdb66b3f3d854c6232c5b77cc995b
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-27 21:04:21 +0900

    Title: 팀으로 하는 틱택토 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240527/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
+++ 240527/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -1,38 +1,35 @@
 import java.util.*;
 
 public class Main {
-    public static int whichCase = 0, idx = 0;
-    public static int[][] tic = new int[3][3];
-    public static int[][] winner = new int[100][2];
+    public static int[][] board = new int[3][3];
+    public static int[][] winComb = new int[100][2];
+    public static int idx = 0;
 
-    public static boolean isWinner(int i, int j) {
-        boolean isTrue = true;
+    public static boolean isTrue(int n1, int n2, int n3) {
+        if(n1 == n2 && n1 != n3) return true;
+        else if(n1 == n3 && n1 != n2) return true;
+        else if(n2 == n3 && n1 != n2) return true;
 
-        for(int k = 0; k < 100; k ++) {
-            if(winner[k][0] == i && winner[k][1] == j) isTrue = false;
-            if(winner[k][0] == j && winner[k][1] == i) isTrue = false;
-        }
-
-        return isTrue;
+        return false;
     }
 
     public static boolean case1(int i, int j) {
-        if(tic[i + 1][j] == tic[i][j] && tic[i + 2][j] != tic[i][j]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i + 2][j] == tic[i][j] && tic[i +1][j] != tic[i][j]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] != tic[i + 1][j] && tic[i + 1][j] == tic[i + 2][j]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
+        int n1 = board[i][j], n2 = board[i][j + 1], n3 = board[i][j + 2];
+
+        if(isTrue(n1, n2, n3)){
+            if(n1 == n3 || n2 == n3) {
+                if(!isWinner(n1, n2)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+            else if(n1 == n2) {
+                if(!isWinner(n1, n3)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+
             return true;
         }
 
@@ -40,22 +37,22 @@ public class Main {
     }
 
     public static boolean case2(int i, int j) {
-        if(tic[i][j] == tic[i][j + 1] && tic[i][j] != tic[i][j + 2]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] == tic[i][j + 2] && tic[i][j] != tic[i][j + 1]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] != tic[i][j + 1] && tic[i][j + 1] == tic[i][j + 2]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
+        int n1 = board[i][j], n2 = board[i + 1][j], n3 = board[i + 2][j];
+
+        if(isTrue(n1, n2, n3)){
+            if(n1 == n3 || n2 == n3) {
+                if(!isWinner(n1, n2)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+            else if(n1 == n2) {
+                if(!isWinner(n1, n3)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+
             return true;
         }
 
@@ -63,103 +60,96 @@ public class Main {
     }
 
     public static boolean case3(int i, int j) {
-        if(tic[i][j] == tic[i + 1][j + 1] && tic[i][j] != tic[i + 2][j + 2]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] == tic[i + 2][j + 2] && tic[i][j] != tic[i + 1][j + 1]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] != tic[i + 1][j + 1] && tic[i + 1][j + 1] == tic[i + 2][j + 2]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
+        int n1 = board[i][j], n2 = board[i + 1][j + 1], n3 = board[i + 2][j + 2];
+
+        if(isTrue(n1, n2, n3)){
+            if(n1 == n3 || n2 == n3) {
+                if(!isWinner(n1, n2)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+            else if(n1 == n2) {
+                if(!isWinner(n1, n3)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+
             return true;
         }
 
-        return false; 
+        return false;
     }
 
     public static boolean case4(int i, int j) {
-        if(tic[i][j] == tic[i + 1][j - 1] && tic[i][j] != tic[i + 2][j - 2]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] == tic[i + 2][j - 2] && tic[i][j] != tic[i + 1][j - 1]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
-            return true;
-        }
-        else if(tic[i][j] != tic[i + 1][j - 1] && tic[i + 1][j - 1] == tic[i + 2][j - 2]) {
-            winner[idx][0] = i;
-            winner[idx][1] = j;
-            idx ++;
+        int n1 = board[i][j], n2 = board[i + 1][j - 1], n3 = board[i + 2][j - 2];
+
+        if(isTrue(n1, n2, n3)){
+            if(n1 == n3 || n2 == n3) {
+                if(!isWinner(n1, n2)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n2;
+                }
+            }
+            else if(n1 == n2) {
+                if(!isWinner(n1, n3)) {
+                    winComb[idx][0] = n1;
+                    winComb[idx ++][1] = n3;
+                }
+            }
+
             return true;
         }
 
-        return false; 
+        return false;
+    }
+
+    public static boolean isWinner(int n1, int n2) {
+        for(int k = 0; k < 100; k ++) {
+            if(winComb[k][0] == n1 && winComb[k][1] == n2) return true;
+            if(winComb[k][0] == n2 && winComb[k][1] == n1) return true;
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int ans = 0;
 
         for(int i = 0; i < 3; i ++) {
-            int col = sc.nextInt();
+            int line = sc.nextInt();
+
             for(int j = 2; j >= 0; j --) {
-                tic[i][j] = col % 10;
-                col /= 10;
+                board[i][j] = line % 10;
+                line /= 10;
             }
         }
 
-        int ans = 0;
-        int idx = 0;
         for(int i = 0; i < 3; i ++) {
             for(int j = 0; j < 3; j ++) {
                 if(i == 0 && j == 0) {
-                    if(case1(i, j) && !isWinner(i, j)){
-                        ans ++;
-                    }
-
-                    if(case2(i, j) && !isWinner(i, j)) {
-                        ans ++;
-                    }
-
-                    if(case3(i, j) && !isWinner(i, j)) {
-                        ans ++;
-                    }
+                    if(case1(i, j)) ans ++;
+                    if(case2(i, j)) ans ++;
+                    if(case3(i, j)) ans ++;
                 }
 
-                else if(i == 0 && j == 1) {
-                    if(case1(i, j) && !isWinner(i, j)) {
-                        ans ++;
-                    }
+                if(i > 0 && j == 0) {
+                    if(case1(i, j)) ans ++;
                 }
 
-                else if(i == 0 && j == 2) {
-                    if(case1(i, j) && !isWinner(i, j)) {
-                        ans ++;
-                    }
-
-                    if(case4(i, j) && !isWinner(i, j)) {
-                        ans ++;
-                    }
+                if(j == 1 && i == 0) {
+                    if(case2(i, j)) ans ++;
                 }
 
-                else if(i > 0 && j == 0) {
-                    if(case2(i, j) && !isWinner(i, j)) ans ++;
+                if(j == 2 && i == 0) {
+                    if(case2(i, j)) ans ++;
+                    if(case4(i, j)) ans ++;
                 }
             }
         }
 
-        System.out.println(ans); 
+        System.out.println(ans);
     }
 }
