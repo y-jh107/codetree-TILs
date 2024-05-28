ed4f7f74d085cc54cbd30a526adbbdf97b0e0cb0
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-28 13:19:05 +0900

    Title: 특정 숫자 도달하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240528/특정 숫자 도달하기/reaching-specific-number.py
+++ 240528/특정 숫자 도달하기/reaching-specific-number.py
@@ -3,7 +3,9 @@ nums = list(map(int, input().split()))
 sum = 0
 for i in range(10):
     if nums[i] >= 250:
-        print("%d %.1f" %(sum, sum / (i)))
         break
 
     sum += nums[i]
+
+
+print("%d %.1f" %(sum, sum / (i)))
