a89f6ac6ef9655435232fe46a5476cce4c01428f
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-05-28 13:20:14 +0900

    Title: 특정 숫자 도달하기, Time: 180ms, Memory: 24MB, Status: Passed - CodeTree

--- 240528/특정 숫자 도달하기/reaching-specific-number.py
+++ 240528/특정 숫자 도달하기/reaching-specific-number.py
@@ -2,9 +2,10 @@ nums = list(map(int, input().split()))
 
 sum = 0
 for i in range(10):
-    devide = i
+    devide = i + 1
 
     if nums[i] >= 250:
+        devide -= 1
         break
 
     sum += nums[i]
