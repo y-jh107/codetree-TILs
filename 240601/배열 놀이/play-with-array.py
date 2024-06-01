f0384d873cdff82c723296572bec48f3cba059db
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-01 15:55:13 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240601/배열 놀이/play-with-array.py
@@ -0,0 +1,19 @@
+n_and_q = list(map(int, input().split()))
+
+n = n_and_q[0]
+q = n_and_q[1]
+
+_list = list(map(int, input().split()))
+
+for i in range(n):
+    question = list(map(int, input().split()))
+    if question[0] == 1:
+        print(_list[question[1] - 1])
+    elif question[0] == 2:
+        index = 0
+        if question[1] in _list:
+            index = _list.index(question[1]) + 1
+        print(index)
+    elif question[0] == 3:
+        for j in range(question[1] - 1, question[2]):
+            print(_list[j], end = ' ')
