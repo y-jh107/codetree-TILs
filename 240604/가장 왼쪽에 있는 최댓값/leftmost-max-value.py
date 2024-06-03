5177a07eedfab0ac12610cb81dcbb5915445755a
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-04 08:46:36 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240604/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240604/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,3 +1,5 @@
+import sys
+
 n = int(input())
 arr = list(map(int, input().split()))
 
@@ -6,15 +8,17 @@ max_index = arr.index(max_value)
 
 print(max_index + 1, end = ' ')
 
-while max_index != 0:
-    max_value = 0
+is_true = True
 
+while is_true:
+    max_value = -sys.maxsize
     for i in range(max_index):
         if arr[i] > max_value:
             max_value = arr[i]
-        
-        max_index = arr.index(max_value)
-        print(max_index + 1, end = ' ')
+    
+    max_index = arr.index(max_value)
+    print(max_index + 1, end = ' ')
 
-        if max_index == 0:
-            break
+    if max_index == 0:
+        is_true = False
+        break
