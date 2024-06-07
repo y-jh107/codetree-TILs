ecc71cc2b042d379e8c3540d2ece7ef27fbadf09
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-07 20:24:17 +0900

    Title: 숫자들의 최대 차, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240607/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static int MAX_N = 1000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(), k = sc.nextInt();
+        int[] nums = new int[n];
+        int ans = 0;
+
+        for(int i = 0; i < n; i ++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i ++) {
+            int idx = 0;
+            int[] arr = new int[n];
+            arr[idx ++] = nums[i];
+
+            for(int j = 0; j < n; j ++) {
+                if(j == i) continue;
+
+                boolean canPut = true;
+
+                for(int l = 0; l < arr.length; l ++) {
+                    if(Math.abs(nums[j] - arr[l]) > k) {
+                        canPut = false;
+                        continue;
+                    }
+                }
+
+                if(canPut) arr[idx ++] = nums[j];
+            }
+
+            ans = Math.max(ans, idx);
+        }
+
+        System.out.println(ans);
+    }
+}
