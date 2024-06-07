ae04b82032d2c4f74e2b088b315a96129f54a3b5
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-07 20:54:20 +0900

    Title: 숫자들의 최대 차, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240607/숫자들의 최대 차/maximum-difference-in-numbers.java
+++ 240607/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     public static int MAX_N = 1000;
+    public static int MAX_NUM = 10000;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,22 +17,31 @@ public class Main {
 
         for(int i = 0; i < n; i ++) {
             int idx = 0;
-            int[] arr = new int[n];
-            arr[idx ++] = nums[i];
+            int[] arr = new int[n];             // 가능한 경우를 따져보기 위한 배열
+            arr[idx ++] = nums[i];              // 첫 번째를 현재 숫자로 넣어둠
 
             for(int j = 0; j < n; j ++) {
                 if(j == i) continue;
 
-                boolean canPut = false;
+                boolean canPut = true;
+                int toPut = nums[j], currMax = 0, currMin = MAX_NUM;            // 넣고 싶은 숫자
 
-                for(int l = 0; l < arr.length; l ++) {
-                    if(Math.abs(nums[j] - arr[l]) > k) {
-                        break;
-                    }
-                    canPut = true;
+                for(int l = 0; l < arr.length; l ++) {      
+                    /* arr 배열 안에 있는 수와 비교해서 k보다 큰 값이 있으면
+                    넣을 수 없으니 canPut을 false로 하고 빠져나온다  */
+                    if(arr[l] == 0) continue;
+                    if(arr[l] > currMax) currMax = arr[l];
+                    if(arr[l] < currMin) currMin = arr[l];
                 }
 
-                if(canPut) arr[idx ++] = nums[j];
+                if(toPut > currMax) currMax = toPut;
+                if(toPut < currMin) currMin = toPut;
+
+                //System.out.println("toput: " + toPut + " currMax: " + currMax + " currMin: " + currMin);
+                
+                if(Math.abs(currMax - currMin) > k) canPut = false;
+
+                if(canPut) arr[idx ++] = toPut;
             }
 
             /*for(int l = 0; l < arr.length; l ++) {
