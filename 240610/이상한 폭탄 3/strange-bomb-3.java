88e1d7e4faf2685a341a00c6e38a6d4c71b6ad82
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-10 14:47:12 +0900

    Title: 이상한 폭탄 3, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240610/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static int MAX_N = 100;
+    public static int[] bomb = new int[MAX_N];
+    public static int n, k;
+    
+    public static boolean isTrue(int x) {
+        boolean isBoom = false;
+
+        for(int i = 0; i < n; i ++) {
+            if(i == x) continue;
+            if(bomb[i] == bomb[x] && Math.abs(i - x) <= 3) isBoom = true; 
+        }
+
+        return isBoom;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        for(int i = 0; i < n; i ++) {
+            bomb[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+        int[] boom = new int[MAX_N];
+
+        for(int i = 0; i < n; i ++) {
+            if(isTrue(i)) {
+                boom[bomb[i]] ++;
+            }
+        }
+
+        for(int i = 0; i < MAX_N; i ++) {
+            if(boom[i] > ans) ans = i;
+        }
+
+        System.out.println(ans);
+    }
+}
