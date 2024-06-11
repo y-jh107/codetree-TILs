7afb9cfa213413065a9d7d1c95ee5b696725c2fa
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-11 21:51:26 +0900

    Title: 초기 수열 복원하기, Time: 324ms, Memory: 30MB, Status: Passed - CodeTree

--- 240611/초기 수열 복원하기/restore-initial-sequence.java
+++ 240611/초기 수열 복원하기/restore-initial-sequence.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static int MAX_N = 1000;
+    public static int MAX_N = 1000 * 100;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
