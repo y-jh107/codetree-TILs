175a9d16513848ece5cbc9d219b44a35dca8b929
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-12 23:18:00 +0900

    Title: 언덕 깎기, Time: 217ms, Memory: 13MB, Status: Passed - CodeTree

--- 240612/언덕 깎기/hill-cutting.java
+++ 240612/언덕 깎기/hill-cutting.java
@@ -15,23 +15,43 @@ public class Main {
 
         Arrays.sort(hill);
 
-        int ans = 0;
-        int topIdx = 0, botIdx = 0;
-        for(int i = 0; i < n / 2; i ++) {
-            if(hill[n - i - 1] - hill[i] <= 17) {
-                topIdx = n - i - 1;
-                botIdx = i;
-                break;
-            }
-        }
+        int ans = Integer.MAX_VALUE;
 
-        for(int i = 0; i < n; i ++) {
-            if(hill[i] < hill[botIdx]) {
-                ans += (hill[botIdx] - hill[i]) * (hill[botIdx] - hill[i]);
+        /*for(int i = 0; i < n; i ++) {
+            for (int j = i + 1; j < n; j++) {
+                if (hill[j] - hill[i] > 17) {
+                    break;
+                }
+
+                int start = hill[i];
+                int end = hill[j];
+                int cost = 0;
+
+                for (int k = 0; k < n; k++) {
+                    if (hill[k] < start) {
+                        cost += Math.pow(start - hill[k], 2);
+                    } else if (hill[k] > end) {
+                        cost += Math.pow(hill[k] - end, 2);
+                    }
+                }
+
+                ans = Math.min(ans, cost);
             }
-            else if(hill[i] > hill[topIdx]) {
-                ans += (hill[i] - hill[topIdx]) * (hill[i] - hill[topIdx]);
+        }*/
+
+        for (int start = 0; start <= 83; start++) {
+            int end = start + 17;
+            int cost = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (hill[i] < start) {
+                    cost += Math.pow(start - hill[i], 2); // 언덕 높이가 구간의 시작보다 낮으면 올리는 비용을 계산합니다.
+                } else if (hill[i] > end) {
+                    cost += Math.pow(hill[i] - end, 2); // 언덕 높이가 구간의 끝보다 높으면 깎는 비용을 계산합니다.
+                }
             }
+
+            ans = Math.min(ans, cost); // 최소 비용을 갱신합니다.
         }
 
         System.out.println(ans);
