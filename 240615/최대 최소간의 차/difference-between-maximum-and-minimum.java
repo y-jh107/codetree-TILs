d0b5c71001fd2e740bb7d2921bd251f34523a69e
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-15 22:17:46 +0900

    Title: 최대 최소간의 차, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240615/최대 최소간의 차/difference-between-maximum-and-minimum.java
+++ 240615/최대 최소간의 차/difference-between-maximum-and-minimum.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     public static int MAX_N = 100;
+    public static int MAX_K = 10000;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,21 +13,16 @@ public class Main {
         for(int i = 0; i < n; i ++)
             arr[i] = sc.nextInt();
 
+        Arrays.sort(arr);
         int ans = Integer.MAX_VALUE;
 
-        for(int i = 0; i <= k; i ++) {
-            int gap = i, cost = 0;
-            int maxVal = arr[0], minVal = arr[0] - gap;
+        for(int i = 1; i <= MAX_K; i ++) {
+            int cost = 0;
+            int minVal = i, maxVal = i + k;
 
             for(int j = 0; j < n; j ++) {
-                if(arr[j] >= maxVal) {
-                    cost += Math.abs(arr[j] - maxVal);
-                    //System.out.println(cost);
-                }
-                else if(arr[j] <= minVal) {
-                    cost += Math.abs(arr[j] - minVal);
-                    //System.out.println(cost);
-                }
+                if(arr[j] > maxVal) cost += Math.abs(arr[j] - maxVal);
+                else if(arr[j] < minVal) cost += Math.abs(minVal - arr[j]);
             }
 
             ans = Math.min(ans, cost);
