83248a45d8741aabc9089bdf195ca27945d35e69
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-18 20:20:59 +0900

    Title: 전부 겹치는 선분 2, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240618/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static int MAX_N = 100;
+    public static int n;
+    public static int[][] line = new int[MAX_N][2];
+
+    public static boolean isColapsed(int x1, int x2) {
+        boolean colapse = true;
+
+        //System.out.println(x1 + " " + x2);
+
+        for(int i = 0; i < n; i ++) {
+            if(line[i][0] == x1 && line[i][1] == x2) continue;
+
+            for(int j = 0; j < n; j ++) {
+                if(j == i || (line[j][0] == x1 && line[j][1] == x2)) continue;
+
+                if(line[i][0] > line[j][1] || line[i][1] < line[j][0]){
+                    //System.out.println(line[i][0] + " " + line[i][1] + " " + line[j][0] + " " + line[j][1]);
+                    colapse = false;
+                    break;
+                }
+            }
+
+            if(!colapse) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i ++) {
+            line[i][0] = sc.nextInt();
+            line[i][1] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i ++) {
+            if(isColapsed(line[i][0], line[i][1])) {
+                System.out.println("Yes");
+                break;
+            }
+        }
+    }
+}
