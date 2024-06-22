1771cadce2aad9c20988a89b323c457c964ea848
y-jh107 <165665548+y-jh107@users.noreply.github.com>
2024-06-22 12:56:07 +0900

    Title: 순위 경쟁2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240622/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static int MAX_N = 100;
+
+    public static int[] score = new int[MAX_N + 1];
+    public static char[] game = new char[MAX_N + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int aScore = 0, bScore = 0;
+
+        for(int i = 0; i < n; i ++) {
+            game[i] = sc.next().charAt(0);
+            score[i] = sc.nextInt();
+        }
+
+        char[] winner = new char[n];
+
+        for(int i = 0; i < n; i ++) {
+            char player = game[i];
+            int diff = score[i];
+
+            if(player == 'A') {
+                aScore += diff;
+            }
+            else if(player == 'B') {
+                bScore += diff;
+            }
+
+            if(aScore < bScore) 
+                winner[i] = 'B';
+            else if(aScore > bScore)
+                winner[i] = 'A';
+            else if(aScore == bScore)
+                winner[i] = 'C';
+        }
+
+        int ans = 1;
+
+        for(int i = 1; i < n; i ++) {
+            if(winner[i] != winner[i - 1]) {
+                ans ++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
